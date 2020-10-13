08a0dbafba047ef9a1ecbb01a8e6edf46685065d
Mark Simunov <mark.simunov@gmail.com>
2020-10-12 20:23:34 -0400

    More practice with TypeScript

--- src/app/tests/tests.component.ts
+++ src/app/tests/tests.component.ts
@@ -19,21 +19,39 @@ export class TestsComponent implements OnInit {
   name: string = "Mark";
   age: number = 22;
   buttonClick: boolean = false;
+  anotherButton: boolean = false;
   buttonClickStatus: string = "The button was not clicked";
-  textName: string = ''; 
+  textName: string = 'text name'; 
   days: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", 
   "Thursday", "Friday", "Saturday"];
+  lineArray: string[] =[];
 
   getAge() {
     return this.age;
   }
 
   onButtonClick() {
-    this.buttonClickStatus = "The button was clicked";
+    this.buttonClickStatus = "Text in input tag is " + this.textName;
   }
 
   onUpdateInputText(event: Event) {
     this.textName = (<HTMLInputElement>event.target).value;
   }
+
+  onAnotherButton() {
+    this.anotherButton = true;
+  }
+
+  getColor() {
+    return this.anotherButton ? 'green' : 'red';
+  }
+
+  onAddLine() {
+    this.lineArray.push("You added a line");
+  }
+
+  
+
+
 
 }
