44857dc02917d74e26da11a44530a981400e45f0
Mark Simunov <mark.simunov@gmail.com>
2020-10-18 12:07:48 -0400

    Added wildcard route and route redirection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,29 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
-import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'sign-in', component: SignInComponent }
-];
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    HomeComponent
+    HomeComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    HttpClientModule
 
   ],
   providers: [],
