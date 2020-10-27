439ad728caca8e578dfc44155290243cbbe2af8b
Mark Simunov <mark.simunov@gmail.com>
2020-10-27 14:24:37 -0400

    Added styling and minor validity checks to sign-in and create account input

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrls: ['./create-account.component.css']
+  styleUrls: ['./create-account.component.css', '../app.component.css']
 })
 export class CreateAccountComponent implements OnInit {
 
@@ -12,4 +12,13 @@ export class CreateAccountComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  username: string = '';
+  password: string = '';
+
+  createAccount() {
+    console.log(this.username);
+    console.log(this.password);
+
+    // We will be sending http requests in this method to firebase; will do later
+  }
 }
