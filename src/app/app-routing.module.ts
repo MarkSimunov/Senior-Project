75cefb5547199d9ef409d480eacfd0eb19046f8a
TimboSlice1106 <tjcampbell@widener.edu>
2020-10-29 05:11:25 -0400

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
 import { CreateAccountComponent } from './create-account/create-account.component';
 import { HomeComponent } from './home/home.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { AboutComponent } from './about/about.component'
 
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "sign-in", component: SignInComponent },
   { path: "create-account", component: CreateAccountComponent },
+  {path: "about", component: AboutComponent }, 
   { path: "**", redirectTo: "/", pathMatch:"full"} //This needs to be the last route in the array
 ];
 
