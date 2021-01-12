0c34ae105b5f6ed1bb7ad2b40bf33e7967d3de49
Mark Simunov <mark.simunov@gmail.com>
2021-01-12 11:12:15 -0500

    Added guard file to commit

---
+++ src/app/account.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AccountService } from './account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountGuard implements CanActivate {
+  
+  constructor(private accountService: AccountService, private router: Router) {
+
+  }
+
+  canActivate(): boolean {
+    if (this.accountService.isLoggedIn) {
+      this.router.navigate(['/']);
+      return false;
+    } else {
+      return true;
+    }
+  }
+  
+}
