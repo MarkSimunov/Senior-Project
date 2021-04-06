808615a4f08fe4f8b52480b42f30910db32d8bca
Mark Simunov <mark.simunov@gmail.com>
2021-04-06 09:45:56 -0400

    Implemented fix where user cannot input question time that is ealier than previous question time

--- src/app/scoring/scoring.component.ts
+++ src/app/scoring/scoring.component.ts
@@ -21,18 +21,53 @@ export class ScoringComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  addTimesToArray(form: NgForm){
+  addTimesToArray(form: NgForm) {
+    if(!form.value.qTime) {
+      window.alert("Must enter in time for question to be submitted at");
+      return;
+    }
     this.startTime = form.value.start;
     this.submissionTimes.push(form.value.qTime);
+    for(var val of this.submissionTimes){
+      if(form.value.qTime < val){
+        window.alert("Cannot answer later question before previous question");
+        this.submissionTimes.pop();
+        return;
+      }
+    }
+    if(this.calculateTotal() < 0) {
+      window.alert("Start time cannot be less than time of answered question");
+      this.submissionTimes.pop();
+      return;
+    }
     this.questionNum++;
-    this.scoreArray.push(this.calculateTotal());
+    this.score = this.calculateTotal();
+    
+    this.scoreArray.push(this.score);
   }
 
   submitScores(form: NgForm) {
+    if(!form.value.qTime) {
+      window.alert("Must enter in time for question to be submitted at");
+      return;
+    }
     this.startTime = form.value.start;
     this.submissionTimes.push(form.value.qTime);
+    if(this.calculateTotal() < 0) {
+      window.alert("Start time cannot be less than time of answered question");
+      this.submissionTimes.pop();
+      return;
+    }
+    for(var val of this.submissionTimes){
+      if(form.value.qTime < val){
+        window.alert("Cannot answer later question before previous question");
+        this.submissionTimes.pop();
+        return;
+      }
+    }
     this.score = this.calculateTotal();
     this.scoreArray.push(this.score);
+    console.log(this.scoreArray)
   }
 
   calculateTotal(): number {
